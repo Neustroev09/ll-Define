acf8e3a2d3e4cbe242a6b895d84c1be7d3508772
U-WIN-3F9R2A34ICF\wind <web.vadim@list.ru>
2022-11-16 15:09:05 +0500

    First Viewer App update

--- entire_app/main.py
+++ entire_app/main.py
@@ -3,6 +3,7 @@
 #apps
 from lldefineapp import LLdefineApp
 from deflvl.deflvl import DefineLevelApp
+from viewer.viewerapp import ViewerApp
 from http_server import HTTPServer
 
 # если данный файл является основным (главный цикл питоновской программы)
@@ -13,9 +14,10 @@ if __name__ == '__main__':
     port = 80
 
     def_lvl_app = DefineLevelApp()
+    viewer_app = ViewerApp()
     #def_lvl_app.init_app()
 
-    our_app_instance = LLdefineApp(def_lvl_app)
+    our_app_instance = LLdefineApp(def_lvl_app, viewer_app)
 
     # создаем его объект
     serv = HTTPServer(host, port, our_app_instance)
