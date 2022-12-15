41a2dc9c93889f091d1a1bab6c9a546ef59b1db5
U-WIN-3F9R2A34ICF\wind <web.vadim@list.ru>
2022-12-16 01:20:59 +0500

    Grammar app update

--- entire_app/grammar/grammar.py
+++ entire_app/grammar/grammar.py
@@ -22,8 +22,8 @@ class GrammarApp:
         nltk.download('brown')
         
     def preprocess(self, text):
-        text = re.sub(r'[^\w\s\,\;]','', text)
-        text = text.replace('cannot', "can not")  
+        #text = re.sub(r'[^\w\s\,\;]','', text)
+        #text = text.replace('cannot', "can not")  
         text2 = nltk.word_tokenize(text)
         tagged_ = nltk.pos_tag(text2)
         return tagged_
@@ -32,7 +32,7 @@ class GrammarApp:
         try:
             tagged_ = self.preprocess(text)
 
-            verb_tags = ['MD','MDF','VB','VBP','VBZ','VBD','VBG','VBN']  
+            verb_tags = ['MD','MDF','VB','VBP','VBZ','VBD','VBG','VBN','TO']  
             verb_phrase = []
 
             for item in tagged_:
@@ -40,33 +40,36 @@ class GrammarApp:
                     verb_phrase.append(item)
 
             grammar = r'''
-                Present Cotinuous: {<VBP><VBG>}
-                Past Continuous: {<VBD><VBG>}
+                Present Cotinuous: {<VBP><VBG>+}
+                Past Continuous: {<VBD><VBG>+}
 
-                Future Continuous: {<MD><VB><VBG>}
+                Future Continuous: {<MD><VB><VBG>+}
 
-                Future Perfect Continuous: {<MD><VB><VBN><VBG>}
-                Future Perfect Continuous_won: {<VBP><VBN><VBG>}
-                Future Perfect Continuous: {<MD><VB|VBP><VBN><VBG>}
+                Future Perfect Continuous: {<MD><VB><VBN><VBG>+}
+                Future Perfect Continuous_won: {<VBP><VBN><VBG>+}
+                Future Perfect Continuous: {<MD><VB|VBP><VBN><VBG>+}
 
-                Future Perfect: {<MD><VB|VBP><VBN>}
-                Future Perfect: {<VBD><PRP><VBP><VBN>}
-                Future Perfect: {<MD><VB|VBP><VBN>}
+                Future Perfect: {<MD><VB|VBP><VBN>+}
+                Future Perfect: {<VBD><PRP><VBP><VBN>+}
+                Future Perfect: {<MD><VB|VBP><VBN>+}
 
-                Present Perfect Continuous: {<VBP|VBZ><VBN><VBG>}
-      
-                Past Perfect Continuous: {<VBD|VBN><VBG>}
+                Present Perfect Continuous: {<VBP|VBZ><VBN><VBG>+}
 
-                Present Perfect: {<VBP|VBZ><VBN>}
-                Present Perfect: {<VBP|VBZ><VBD|VBN>}
+                Past Perfect Continuous: {<VBD|VBN><VBG>+}
 
-                Past Perfect Continuous: {<VBD><VBN><VBG>}
-                Past Perfect: {<VBD|VBP><VBN|VB|VBP>}
-           
-                Future Simple: {<MD><VB>}
-                Future Simple: {<MD><VB|VBP|VBZ>}
-                Present Simple: {<VBP|VBZ|VB>}  
-                Past Simple: {<VBD>}      
+                Present Perfect: {<VBP|VBZ><VBN>+}
+                Present Perfect: {<VBP|VBZ><VBD|VBN>+}
+
+                Past Perfect Continuous: {<VBD><VBN><VBG>+}
+                Past Perfect: {<VBD|VBP><VBN|VBP>+}
+               
+                Future Simple: {<MD><VB>+}
+                Future Simple: {<MD><VB|VBP|VBZ>+}
+                
+                Past Simple: {<VBD><TO><VB>}  
+                Present Simple: {<VBP|VBZ><TO><VB>+}
+                Present Simple: {<VBP|VBZ|VB>+}
+                Past Simple: {<VBD|VBN>} 
             '''
             
             cp = nltk.RegexpParser(grammar)
@@ -85,9 +88,9 @@ class GrammarApp:
             return []
 
     def tok_pos(self, predl):  
-      
+
         text = re.sub(r'[^\w\s\,\;]','', predl)
-        tokens_positions = list(WhitespaceTokenizer().span_tokenize(text))  
+        tokens_positions = list(WhitespaceTokenizer().span_tokenize(predl))  
         tokens = WhitespaceTokenizer().tokenize(text)  
 
         tokens = nltk.pos_tag(tokens) 
@@ -98,14 +101,25 @@ class GrammarApp:
             start, end = tokens_positions[i]  
             if tag in ['MD','MDF', 'VB','VBP','VBZ','VBD','VBG','VBN']:
                 words.append((start, end, tag, text))
-
         return words
         
     def tenses(self, text):
         
         rt = text
         
-        f1_r = self.def_tense(text)
+        text1 = re.sub("must|have to|would|could|may|might|to have|could|can|shall", "", text)
+        text1 = re.split(";|,|\n|-|:|and\s|for\s|but\s|or\s|yet\s|or\s|so\s|otherwise\s|while\s|else\s|for\s|whereas\s|if\s|that\s|\sas\s|because\s|what\s|whom\s|if\s|why\s|when\s|where\s|unless\s", text1)
+        
+        masive = []
+        for i in range (len(text1)):
+            if text1[i] == ' ':
+                continue
+            else:  
+                result2 = self.def_tense(text1[i])
+                if result2 != []:
+                    masive.extend(result2)
+        
+        f1_r = masive
         f2_r = self.tok_pos(text)
         
         real_res = []
